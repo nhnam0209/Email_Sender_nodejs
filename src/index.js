7ffa00fa74544abda27f3f4b0214977fbd05fc6b
nhnam0209 <93809958+nhnam0209@users.noreply.github.com>
2022-02-20 12:29:51 +0700

    Using on windows platform but if the account have auth

--- src/index.js
+++ src/index.js
@@ -1,22 +1,20 @@
-let client = require('@sendgrid/mail')
+let client = require("@sendgrid/mail");
 
-client.setApiKey(process.env.SENDGRID_API_KEY)
 
-client.send({
-    to:{
-        email: process.env.EMAIL_RECEIVER,
-        name: "Nguyen Hoang Nam",
-    },
-    from :{
-        email: process.env.EMAIL_RECEIVER,
-        name: "Nguyen Hoang Nam",
-    },
+client.setApiKey(process.env.SENDGRID_API_KEY);
+
+const msg = {
+    to: 'nhnam3004@gmail.com', // Change to your recipient
+    from: 'nhnam3004@gmail.com', // Change to your verified sender
     templateId: 'ad18c2c4a61640abb6676ce244e74997',
-    dynamicTemplateData:{
-        name: "Nguyen Hoang Nam!!"
-    },
-})
+  }
+  client
+    .send(msg)
+    .then(() => {
+      console.log('Email sent')
+    })
+    .catch((error) => {
+      console.error(error)
+    })
 
-.then(() =>{
-    console.log("Email was sent!");
-});
+
