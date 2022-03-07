a289a83c52030353ed2b1329ad9f5ae4b2a99e91
nhnam0209 <93809958+nhnam0209@users.noreply.github.com>
2022-03-07 09:18:02 +0700

    update

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ let client = require("@sendgrid/mail");
 client.setApiKey(process.env.SENDGRID_API_KEY);
 
 const msg = {
-    to: 'nhnam3004@gmail.com', // Change to your recipient
-    from: 'nhnam3004@gmail.com', // Change to your verified sender
-    templateId: 'ad18c2c4a61640abb6676ce244e74997',
+    to: '<email_receiver>', // Change to your recipient
+    from: '<email_sender>', // Change to your verified sender
+    templateId: '<template_key>',
   }
   client
     .send(msg)
